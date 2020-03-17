73f3226808021f70f50133308e02ac1be063f5b2
sankar-wa <ssankarwa@gmail.com>
2020-03-17 10:31:56 +0530

    resolved conflict

--- src/components/explore/explore.js
+++ src/components/explore/explore.js
@@ -7,12 +7,7 @@ getUsersById(parseInt(localStorage.getItem("UserId"))
 ).then(data => {
     if(data == undefined){
         return false;
-<<<<<<< HEAD
     }   
-=======
-    }
-    // debugger;
->>>>>>> 70d1b2574d34e477197f8fd505af8258f22abd05
     $("#usernameid").text(data[0].FirstName)
     $("#userimage").attr('src',data[0].userimage);
 }).catch(err => {
