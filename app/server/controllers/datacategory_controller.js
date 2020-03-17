e315af1cc02aa5546956147264d5e09469dc10e4
nikunjg <nikunjg.wa@gmail.com>
2020-03-17 15:53:14 +0530

    added data point insert

---
+++ app/server/controllers/datacategory_controller.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const { Datacategory } = require('../models')
+const {
+    createError,
+    BAD_REQUEST,
+    UNAUTHORIZED
+} = require('../helpers/error_helper')
+
+const getDatacategory = () => {
+    return Datacategory.findAll();
+}
+
+
+module.exports = {
+    getDatacategory
+}
---
+++ app/server/models/datacategory.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const createGuts = require('../helpers/model-guts')
+
+const name = 'Datacategory'
+const tableName = 'datacategory'
+
+const selectableProps = [
+    'Id',
+    'Name',
+    'IsActive'
+]
+
+module.exports = knex => {
+    const guts = createGuts({
+        knex,
+        name,
+        tableName,
+        selectableProps
+    })
+
+    return {
+        ...guts
+    }
+}
