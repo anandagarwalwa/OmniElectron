49ad6f16abb20fd9267d4cb2e7ff7037386f22ef
sankar-wa <ssankarwa@gmail.com>
2020-04-04 16:30:34 +0530

    added hide node on filter click

--- src/components/explore/explore.js
+++ src/components/explore/explore.js
@@ -31,6 +31,14 @@ $(function () {
         if (e.keyCode != 13) {
             serchExplore();
         }
+        else{
+            if (isNodeFilter) {
+                NodeFilterGraphData(null,$('#txtSearch').val());
+            }
+            else {
+                FilterGraphBySearchPanel(null,$('#txtSearch').val());
+            }
+        }
     });
     var isClearClick = false, filterId = '';
     $('body').on('click', 'a.dynamic-box', function () {
@@ -43,14 +51,24 @@ $(function () {
             $(this).addClass("active");
             isClearClick = true;
             filterId = $(this).attr("data-val");
-            FilterGraphBySearchPanel($(this).attr("data-val"));
+            
+            if (isNodeFilter) {
+                NodeFilterGraphData(filterId,null);
+            }
+            else {
+                FilterGraphBySearchPanel(filterId,null);
+            }
         }
     });
     $("#explorenode").click(function () {
+        isClearClick = false, filterId = '';
+        Bind2DForceGraph();
         isNodeFilter = false;
     });
     $("#fildernode").click(function () {
+        isClearClick = false, filterId = '';
         isNodeFilter = true;
+        NodeFilterGraphData();
     });
 });
 function serchExplore() {
@@ -263,13 +281,13 @@ function Bind2DForceGraph() {
             ctx.textBaseline = 'middle';
             ctx.fillStyle = node.color;
             ctx.fillStyle = '#54545f';
-            ctx.fillText(label, node.x, node.y + bckgDimensions[1] + 1);            
+            ctx.fillText(label, node.x, node.y + bckgDimensions[1] + 1);
         })
         .onNodeHover(node => {
             elem.style.cursor = node ? 'pointer' : null
         })
         .onNodeClick(node => {
-            if(isNodeFilter){
+            if (isNodeFilter) {
                 $('#myModal').modal('show');
                 getNodeFilterData(node.nodeId).then(data => {
                     var filterData = data[0];
@@ -280,7 +298,7 @@ function Bind2DForceGraph() {
                     codeLink = filterData[0].lCodeLink;
                     reportLink = filterData[0].lReportLink;
                 })
-            }            
+            }
             // Center/zoom on node
             Graph.centerAt(node.x, node.y, 1000);
             Graph.zoom(8, 2000);
@@ -299,7 +317,7 @@ function updateHighlight(filterColor) {
             .centerAt(node.x, node.y, 1000)
             ;
         Graph.zoom(7, 2000);
-    }
+    }    
 }
 
 function FilterGraphBySearchPanel(selId) {
@@ -346,3 +364,100 @@ $("#reportlink").click(function () {
     const { shell } = require('electron') // deconstructing assignment
     shell.showItemInFolder(reportLink)
 });
+
+function NodeFilterGraphData(selId, searchText) {
+    //var searchText = $('#txtSearch').val();   
+    if (searchText) {
+        highlightNodes = $.grep(nodes, function (n, i) {
+            return (n.id.indexOf(searchText) > -1);
+        });
+        highlightLink = $.grep(links, function (n, i) {
+            return (n.value.indexOf(searchText) > -1);
+        });
+    }
+    else if(selId) {
+        var selectedBreakDownVal = parseInt($('#ddlBreakDown').val());
+        var prop = "";
+        selId = parseInt(selId);
+        switch (selectedBreakDownVal) {
+            case BreakdownEnum.Channel:
+                prop = "channelId";
+                break;
+            case BreakdownEnum.Team:
+                prop = "teamId";
+                break;
+            case BreakdownEnum.DataTool:
+                prop = "dataToolId";
+                break;
+        }
+        var filteredLinks = $.grep(links, function (v) {
+            return v[prop] === selId;
+        });
+        highlightNodes = [], highlightLink = [];
+        if (filteredLinks) {
+            var linkColor = '';
+            filteredLinks.forEach(element => {
+                var filteredLNode = $.grep(nodes, function (v) {
+                    return v.nodeId === element.nodeId;
+                });
+                highlightNodes.push(filteredLNode[0]);
+                highlightLink.push(element);
+                linkColor = element.linkColor;
+            });
+        }
+    }
+    else{
+        highlightNodes = nodes;
+        highlightLink = links;
+    }
+
+    graphData.nodes = highlightNodes;
+    graphData.links = highlightLink;
+    const elem = document.getElementById('graph');
+    Graph = ForceGraph()
+        (elem)
+        .width($("#graph").width())
+        .height(window.innerHeight - 100)
+        // .backgroundColor("#000011")
+        .graphData(graphData)
+        .nodeLabel('id')
+        .nodeColor(d => d.nodeColor)
+        .nodeVal(d => d.nodeSize)
+        .linkColor(link => link.linkColor)
+        .nodeCanvasObjectMode(node => 'after')
+        .nodeCanvasObject((node, ctx, globalScale) => {
+            const label = node.id;
+            const fontSize = 12 / globalScale;
+            ctx.font = `${fontSize}px Arial`;
+            const textWidth = ctx.measureText(label).width;
+            const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+            //ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+            //ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillStyle = node.color;
+            ctx.fillStyle = '#54545f';
+            //   ctx.fillText(label, node.x, node.y);
+            ctx.fillText(label, node.x, node.y + bckgDimensions[1] + 1);
+            // ctx.fillText(label, node.x, node.y + (fontSize * 0.8));
+        })
+        .onNodeHover(node => {
+            elem.style.cursor = node ? 'pointer' : null
+        })
+        .onNodeClick(node => {
+            if (isNodeFilter) {
+                $('#myModal').modal('show');
+                getNodeFilterData(node.nodeId).then(data => {
+                    var filterData = data[0];
+                    $("#filterusername").text(filterData[0].uFirstname + " " + filterData[0].uLastName);
+                    $("#nodeDescription").text(filterData[0].nDescription);
+                    $("#filterDescription").text(filterData[0].lLinkDescription);
+                    $("#filterTags").text(filterData[0].lTags);
+                    codeLink = filterData[0].lCodeLink;
+                    reportLink = filterData[0].lReportLink;
+                })
+            }
+            Graph.centerAt(node.x, node.y, 1000);
+            Graph.zoom(8, 2000);
+        });
+}
