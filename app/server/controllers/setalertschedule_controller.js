45144c578f4cc9960273cee2eb6d77214a86e8e2
sankar-wa <ssankarwa@gmail.com>
2020-06-06 15:00:01 +0530

    Change Query

--- app/server/controllers/setalertschedule_controller.js
+++ app/server/controllers/setalertschedule_controller.js
@@ -44,8 +44,10 @@ const deleteAlertschedulebyID = (nodeId) => {
 }
 
 const getAlerSchedulerList = (userId) => {
-    let query = "SELECT * FROM alertschedule AS alert_sch " +
-        "INNER JOIN alertmaster as alert_master ON alert_master.AlertId = alert_sch.Id";
+    let query = "SELECT alert_sch.*,alert_master.*,l.Location,d.Name as DataSouceName,l.DataSourceId FROM alertschedule AS alert_sch " +
+        "INNER JOIN alertmaster as alert_master ON alert_master.AlertId = alert_sch.Id "+
+        "INNER JOIN links as l ON l.NodeId = alert_sch.NodeId " + 
+        "LEFT JOIN datasources d on d.Id=l.DataSourceId ";
     if (userId)
         query += ' where alert_sch.UserId=' + userId + ' and alert_master.CreatedBy =' + userId;
     return Alertschedule.raw(query);
